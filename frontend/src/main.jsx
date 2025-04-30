eb327000672aa4b5c5882b268e200812ac6e7469
Keaton Murray <murraykeaton422@gmail.com>
2025-04-30 12:07:44 -0400

    Created Navbar skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ApolloProvider } from '@apollo/client'
-import client from './apolloClient'
+import client from './apollo'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
