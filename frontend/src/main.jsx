088b3225a0a650196bdd394717394db8a7be77ca
Keaton Murray <murraykeaton422@gmail.com>
2025-04-30 12:43:03 -0400

    Installed and imported Bootstrap and Bootstrap Icons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import client from './apollo';
 import './assets/css/style.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+import App from './App';
 import Home from './pages/Home'; 
 import SingleProduct from './pages/SingleProduct';
 
