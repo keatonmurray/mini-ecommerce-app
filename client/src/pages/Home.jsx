faec8f1e3c7e9381089cb76a0c7a74bf163bf827
Keaton Murray <murraykeaton422@gmail.com>
2025-05-07 06:28:31 -0400

    Created .env file. Products data fetching successful

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
-const items = [
-  { id: 1, title: 'Clothes', price: 50, image: '/images/women.png' },
-  { id: 2, title: 'Clothes', price: 50, image: '/images/women.png' },
-  { id: 3, title: 'Clothes', price: 50, image: '/images/women.png' },
-  { id: 4, title: 'Clothes', price: 50, image: '/images/women.png' },
-  { id: 5, title: 'Clothes', price: 50, image: '/images/women.png' }
-];
+import axios from 'axios';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.post(import.meta.env.VITE_API_URL, {
+        query: `
+          {
+            products {
+              name
+            }
+          }
+        `
+      });
+
+      setProducts(response.data.data.products);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="home">
-      <h4 className="page-title mt-5  px-md-auto px-3">Women</h4>
+      <h4 className="page-title mt-5 px-md-auto px-3">Products</h4>
       <div className="row text-center">
-        {items.map((item) => (
+        {products.map((product, index) => (
           <div
-            key={item.id}
+            key={index}
             className="col-12 col-md-4 d-flex align-items-center justify-content-center"
           >
             <Link to="/product" className="text-decoration-none">
               <div className="item mt-5 w-100">
                 <div className="m-0 w-100 px-md-auto px-3">
-                  <img src={item.image} alt={item.title} className="img-fluid w-100" />
+                  <img src="/images/women.png" alt={product.name} className="img-fluid w-100" />
                   <div className="text-start mt-3">
                     <p className="product-name">
-                      {item.title}
-                      <span className="d-block fw-bold">${item.price}</span>
+                      {product.name}
+                      <span className="d-block fw-bold">$99</span> {/* Placeholder price */}
                     </p>
                   </div>
                 </div>
